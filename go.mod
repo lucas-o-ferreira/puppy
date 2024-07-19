cc18a0581be97f856bef4e77539a88fe0925b50e
Lucas Ferreira <134474361+lucas-o-ferreira@users.noreply.github.com>
2024-07-18 21:43:58 -0300

    dog dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lucas-o-ferreira/puppy
 
 go 1.22.5
+
+require github.com/lucas-o-ferreira/dog v0.0.0-20240719002316-2b193736e354 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/lucas-o-ferreira/dog"
+
 func Bark() string {
 	return "Bark!"
 }
 
 func Barks() string {
 	return "Bark! Bark! Bark!"
+}
+
+func BigBark() {
+	dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() {
+	dog.WhenGrownUp(Barks())
 }
